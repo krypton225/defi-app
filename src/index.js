56d9a618d3d5c73dd0f9080f0b56dc3c9cc046da
krypton225 <k.mohamed.11.98@gmail.com>
2023-01-17 18:44:28 +0200

    ADD Nunito font files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import "./index.css";
+
 import App from './App';
 
 const main = ReactDOM.createRoot(document.getElementById('main'));
