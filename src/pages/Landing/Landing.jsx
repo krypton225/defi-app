6d823f8924ac90ef67a853d8cdbbf135bbea1a45
krypton225 <k.mohamed.11.98@gmail.com>
2023-01-18 08:18:46 +0200

    ADD Top section @ Landing route

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,31 @@
-import { LandingStyle } from "./Landing.styles";
+import Container from "../../utils/container";
+
+import { LandingStyle, LandingContent, LeftLandingContent, DescriptionLeftLandingContent, TitleLeftLandingContent, RightLandingContent } from "./Landing.styles";
+
+import LandingImage from "../../assets/images/hero-img.png";
 
 const Landing = () => {
     return (
         <LandingStyle>
-            Landing
+            <Container>
+                <LandingContent>
+                    <LeftLandingContent>
+                        <DescriptionLeftLandingContent>
+                            buy & sell crypto 24/7 using your retirement account
+                        </DescriptionLeftLandingContent>
+
+                        <TitleLeftLandingContent>invest in cryptocurrency with your IRA</TitleLeftLandingContent>
+
+                        <DescriptionLeftLandingContent>
+                            buy, sell and store hundreds of cryptocurrencies
+                        </DescriptionLeftLandingContent>
+                    </LeftLandingContent>
+
+                    <RightLandingContent>
+                        <img src={LandingImage} alt="Hero Landing" draggable="false" />
+                    </RightLandingContent>
+                </LandingContent>
+            </Container>
         </LandingStyle>
     )
 }
--- src/pages/Landing/Landing.styles.js
+++ src/pages/Landing/Landing.styles.js
@@ -3,8 +3,68 @@ import styled from "styled-components";
 /* *
 * =========== LandingStyle =========== *
 **/
-
 export const LandingStyle = styled.section`
     width: 100%;
     height: 100vh;
+    /* background-color: blue; */
+`;
+
+/* *
+* =========== LandingContent =========== *
+**/
+export const LandingContent = styled.div`
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template-columns: 48% 48%;
+    align-items: center;
+    gap: 4%;
+    /* background-color: red; */
+
+    @media (max-width: 840px) {
+        grid-template-columns: 100%;
+        gap: 5rem;
+        text-align: center;
+    }
+`;
+
+/* *
+* =========== LeftLandingContent =========== *
+**/
+export const LeftLandingContent = styled.div`
+    width: 100%;
+`;
+
+/* *
+* =========== DescriptionLeftLandingContent =========== *
+**/
+export const DescriptionLeftLandingContent = styled.p`
+    font-size: 1.7rem;
+    text-transform: capitalize;
+`;
+
+/* *
+* =========== TitleLeftLandingContent =========== *
+**/
+export const TitleLeftLandingContent = styled.h2`
+    margin-block: 0.5rem;
+    font-weight: var(--fw-700);
+    font-size: 4rem;
+    text-transform: capitalize;
+`;
+
+/* *
+* =========== RightLandingContent =========== *
+**/
+export const RightLandingContent = styled.div`
+    width: 33rem;
+    margin-inline: auto;
+
+    @media (min-width: 768px) {
+        width: 38rem;
+    }
+
+    @media (min-width: 991px) {
+        width: 40rem;
+    }
 `;
