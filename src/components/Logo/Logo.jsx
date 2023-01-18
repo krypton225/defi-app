3790c2893a9ba2d0083e9db3f05f189c04d649c4
krypton225 <k.mohamed.11.98@gmail.com>
2023-01-18 07:51:54 +0200

    EDIT link Logo in react-router-dom Link comp

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import { LogoImageStyle, MainLogo } from "./Logo.styles";
 
 import LogoImage from "../../assets/svg/logo.svg";
@@ -5,11 +7,11 @@ import LogoImage from "../../assets/svg/logo.svg";
 const Logo = () => {
     return (
         <MainLogo>
-            <a href="/">
+            <Link to="/">
                 <LogoImageStyle>
                     <img src={LogoImage} alt="Logo" />
                 </LogoImageStyle>
-            </a>
+            </Link>
         </MainLogo>
     )
 }
